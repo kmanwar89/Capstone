b06d5f8e6b3daa2077cfe684d5a454cd1a333565
Kadar Anwar <anwarkm@dukes.jmu.edu>
2017-02-24 15:43:50 +0000

    Publish stress test files added

--- StressTesting/publishstress.py
+++ StressTesting/publishstress.py
@@ -1,15 +1,116 @@
-#! /bin/python
-import paho.mqtt.publish as publish
+#!/usr/bin/python
 
-#publish.connect(192.168.99.75, port=1884, keepalive=60,  bind_address="")
+# MQTT Publish Stress Test
 
-#publish.subscribe(Capstone)
+# Purpose: Stress test MQTT broker with publishing thousands of messages.
 
-#publish.single(Capstone, payload="Test", hostname="192.168.99.75", \
-#	port=1884, client_id="stress")
+# Author: Kadar M. Anwar
+# Language: Python 3.5
+# Date: 2-23-2017
+# JMU ISAT Senior Capstone Project
 
-x = 1
-while True:
-	msgs=[{'topic':"Capstone", 'payload':"Stress test string"}]
-	publish.multiple(msgs, hostname="192.168.99.75")
-#	time.sleep(2)
+# v0.1 - 2/23/2017 - initial code
+
+# Import libraries
+from sense_hat import SenseHat
+import paho.mqtt.client as mqtt
+import time
+
+# Instantiate objects
+client = mqtt.Client()
+sense = SenseHat()
+
+# Connect to the broker over MQTT port
+client.connect("192.168.99.75", port=1883, keepalive=60)
+client.loop_start() # Keep the connection open
+
+# Get temperature from sensor and convert it to Farenheit
+tempC = sense.get_temperature()
+tempF = (tempC * 1.8) + 32
+#tempF = format('.2f', tempF)
+
+# Publish a message to the "Capstone" topic
+#client.publish("Capstone", payload=tempF, qos=0)
+
+# Loop the stress test
+z = 0
+for x in range(0,1001):
+	for y in range (0,101):
+		client.publish("Capstone", payload = tempF, qos=0)
+		z+=1
+		print(z, "packets sent")
+
+#print("Packets sent successfully: ", x*y)
+#print("Packets sent total: ", z)
+############################################
+# Additional callbacks, not used currently #
+############################################
+# def on_publish(client, userdata, mid):
+#     print("mid: "+str(mid))
+#def on_connect(client, userdata, rc):
+#    print("Connection Result: "+connack_string(rc))
+#client.on_publish = on_publish
+#client.on_connect = on_connect
+
+
+#	humidity = sense.get_humidity()
+#	pressure = sense.get_pressure()
+#	psi = (pressure * 0.0145038)
+
+
+
+#    time.sleep(30)
+    #temperature = read_from_imaginary_thermometer()
+################
+
+# ######################
+# More code to combine #
+# ######################
+# import paho.mqtt.client as paho
+#
+# def on_subscribe(client, userdata, mid, granted_qos):
+#     print("Subscribed: "+str(mid)+" "+str(granted_qos))
+#
+# def on_message(client, userdata, msg):
+#     print(msg.topic+" "+str(msg.qos)+" "+str(msg.payload))
+#
+# client = paho.Client()
+# client.on_subscribe = on_subscribe
+# client.on_message = on_message
+# client.connect("192.168.99.75", 1883)
+# client.subscribe("Capstone")
+#
+# client.loop_forever()
+############################
+
+# Keep the connection open
+#client.loop_forever()
+
+# disconnect
+#client.disconnect()
+
+# while True:
+#     client = new MqttClient("tcp://192.168.99.71:1883", "ClientIdentifier");
+#     client.connect();
+#     MqttMessage message = new MqttMessage();
+#     message.setPayload("A single message".getBytes());
+#     client.publish("Capstone", message);
+#client.disconnect();
+
+
+# SSL/TLS support, not currently used.
+# tls_set(ca_certs, certfile=None, keyfile=None, cert_reqs=ssl.CERT_REQUIRED,
+#     tls_version=ssl.PROTOCOL_TLSv1, ciphers=None)
+
+## Interesting function we may be able to use in our vulnerability
+# scenarios
+# tls_insecure_set()
+#
+# tls_insecure_set(value)
+# Configure verification of the server hostname in the server certificate.
+#
+# If value is set to True, it is impossible to guarantee that the host you are connecting to is not impersonating your server. This can be useful in initial server testing, but makes it possible for a malicious third party to impersonate your server through DNS spoofing, for example.
+#
+# Do not use this function in a real system. Setting value to True means there is no point using encryption.
+#
+# Must be called before connect*).
