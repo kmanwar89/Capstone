d706a9f22be8420c4fad51faa47da72063a2fca9
Kadar Anwar <anwarkm@dukes.jmu.edu>
2017-04-14 08:42:03 -0400

    Adding additional environmental values

--- Paho_Python/PublishPaho.py
+++ Paho_Python/PublishPaho.py
@@ -9,7 +9,8 @@
 
 # v0.1 - 2/22/2017 - initial code
 # v0.2 - 2/23/2017 - combining code from publish2.py and publish.py into a single file
-# v0.3 - 4/14/2017 - updating code for Dry Run #2
+# v0.3 - 4/14/2017 - updating code for Dry Run #2, combined data gathering
+#	and temp conversion with formatting into a single string
 
 # Import libraries
 from sense_hat import SenseHat
@@ -21,18 +22,23 @@ client = mqtt.Client()
 sense = SenseHat()
 
 # Connect to the broker over MQTT port
-client.username_pw_set(admin,password=admin)
+#client.username_pw_set(admin,password=admin)
 client.connect("192.168.99.135", port=1883, keepalive=60)
 client.loop_start() # Keep the connection open
 
 # Get temperature from sensor and convert it to Farenheit
-tempC = sense.get_temperature()
-tempF = (tempC * 1.8) + 32
-tempF = format('.2f', tempF)
+tempF = format(((sense.get_temperature() * 1.8) + 32), '.2f')
+humidity = sense.get_humidity()
+psi = format(sense.get_pressure() * 0.0145038, '.2f')
 
 # Publish a message to the "Capstone" topic
-client.publish("Capstone", payload=tempF, qos=0)
-
 while True:
-    client.publish("Capstone", payload = tempF, qos=0)
+    data = [{
+
+
+print("Current environmental conditions:" \
+		+ ' ' + "Temperature:" + str(tempF) \
+		+ ' ' + "% Relative Humidity" + str(humidity) \
+		+ ' ' + "Pressure (PSI)" + str(psi))
+    client.publish("Capstone", payload = data, qos=0)
     time.sleep(1)
