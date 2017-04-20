daf95fb2349f31f50d6cc777359011ba8fd0484b
Kadar Anwar <anwarkm@dukes.jmu.edu>
2017-04-19 21:34:24 -0400

    Updates

--- StressTesting/dos.py
+++ StressTesting/dos.py
@@ -5,5 +5,5 @@ import subprocess, sys, os
 #for i in range(0,10):
 #subprocess.call(['./StressPublishPaho.py'])
 
-os.system('python3.5 "test.py"')
+os.system('python3.5 "test2.py"')
 #os.system('echo "Test"')
